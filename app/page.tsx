ae6eae905f12e65777ace4798a2cfca96b22833c
Deepak Mohapatra <deepakmohapatra12@gmail.com>
2024-11-02 18:25:23 +0530

    config changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
 
 const Home = () => (
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to the world of Next js.</h1>
+    <h1 className="text-3xl font-black text-violet-700">Welcome to the world of Next js.</h1>
   );
 
 export default Home;
