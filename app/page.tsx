9ca0facbfbca2f30fc164d8fe5104958f80f5a20
Deepak Mohapatra <deepakmohapatra12@gmail.com>
2024-11-02 18:45:09 +0530

    tailwind css theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 
 const Home = () => (
-    <h1 className="text-3xl font-black text-violet-700">Welcome to the world of Next js.</h1>
+    <h1 className="h1-bold">Welcome to the world of Next js.</h1>
   );
 
 export default Home;
